4272a970d86a4b49b12cacb187c6541da0b3cd2e
BGA360 <BGA360@users.noreply.github.com>
2026-07-01 07:45:36 +0200

    Configure Astro and GitHub Actions for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bridgenta.de',
   // Enable static site generation (default in Astro)
   output: 'static'
 });
