3654d36adccf577df2d446c27bfb873cf0237514
BGA360 <BGA360@users.noreply.github.com>
2026-07-26 10:40:29 +0200

    fix: integrate tinacms build into package.json build script to resolve 404 admin page

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'null', queries,  });
+export const client = createClient({ cacheDir: 'C:/antigravity/statichtmlpro/fdrefs/tina/__generated__/.cache/1785055106828', url: 'http://localhost:4001/graphql', token: 'null', queries,  });
 export default client;
   
