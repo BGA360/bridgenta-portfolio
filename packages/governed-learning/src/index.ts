96a5c2f5af71d8aba093aa4d103b42bf75686180
BGA360 <BGA360@users.noreply.github.com>
2026-09-19 07:15:29 +0200

    feat(governed-learning): add Wave 9 runtime composition and public exports

--- packages/governed-learning/src/index.ts
+++ packages/governed-learning/src/index.ts
@@ -2,3 +2,5 @@ export * from './types/enums.js';
 export * from './types/primitives.js';
 export * from './contracts/index.js';
 export * from './helpers/index.js';
+export * from './runtime/index.js';
+
